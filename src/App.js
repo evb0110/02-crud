9161662d6748e5409b2faf3d32f02ba3c53501b5
Eugene Barsky <34789976+evb0110@users.noreply.github.com>
2019-08-05 21:15:43 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import axios from 'axios';
+
+import Notes from './components/Notes';
+
+class App extends Component {
+  state = {
+    notes: [],
+    value: '',
+  };
+
+  constructor() {
+    super();
+    this.getNotes = this.getNotes.bind(this);
+    this.delNote = this.delNote.bind(this);
+    this.notesURL = 'http://localhost:7777/notes';
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ ...this.state, value: e.target.value });
+  }
+
+  getNotes() {
+    axios
+      .get(this.notesURL)
+      .then(response => this.setState({ notes: response.data }));
+  }
+
+  componentDidMount() {
+    this.getNotes();
+  }
+
+  delNote(id) {
+    axios.delete(`${this.notesURL}/${id}`).then(() => this.getNotes());
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const id = Math.random()
+      .toString()
+      .slice(2, 10);
+    const newNote = { id, content: this.state.value };
+    axios.post(this.notesURL, newNote).then(() => {
+      this.getNotes();
+      this.setState({ ...this.state, value: '' });
+    });
+  }
+
+  render() {
+    return (
+      <div className="app-container">
+        <form onSubmit={this.handleSubmit} className="enter-new">
+          <textarea
+            rows="3"
+            cols="50"
+            placeholder="Enter new note"
+            onChange={this.handleChange}
+            value={this.state.value}
+          />
+          <button>Submit</button>
+        </form>
+        <div className="App">
+          <Notes delNote={this.delNote} notes={this.state.notes} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
